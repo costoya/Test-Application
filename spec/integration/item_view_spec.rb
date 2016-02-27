34cc48a54b40a9a814841cd9ad259962f19a5556
costoya <acostoyal@gmail.com>
2016-02-27 17:51:54 +0100

    Used Let instead of the global variable

--- spec/integration/item_view_spec.rb
+++ spec/integration/item_view_spec.rb
@@ -15,7 +15,7 @@ describe 'Insert element', type: :feature do
   end
 
   it 'is happy path' do
-    Rails.logger.info("New Item – Happy path")
+    Rails.logger.info('New Item – Happy path')
     time = Time.now
     item.name = 'xName' + time.inspect
     item.description = 'xDescription' + time.inspect
@@ -24,25 +24,25 @@ describe 'Insert element', type: :feature do
   end
 
   it 'has strange characters' do
-    Rails.logger.info("New Item – Happy path")
+    Rails.logger.info('New Item – Happy path')
     time = Time.now
     item.name = "漢字áéñç<a href='http://www.xing.com>test</a>'" + time.inspect
     item.description = "漢字áéñç<a href='http://www.xing.com>test</a>'" + time.inspect
     createPage(item)
     checkValid(item)
-    $name = "漢字áéñç"
+    item.name = '漢字áéñç'
   end
 
   it 'puts empty values' do
-    Rails.logger.info("New Item – Empty values")
+    Rails.logger.info('New Item – Empty values')
     item.name = ''
     item.description = ''
     createPage(item)
-    expect(page).to have_content("3 errors prohibited this item from being saved:");
+    expect(page).to have_content('3 errors prohibited this item from being saved:');
   end
 
   it 'has Huge size' do
-    Rails.logger.info("New Item – Huge size")
+    Rails.logger.info('New Item – Huge size')
     time = Time.now
     item.name = '123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890' + time.inspect
     item.description = '123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890' + time.inspect
@@ -51,22 +51,26 @@ describe 'Insert element', type: :feature do
   end
 
   it 'is cancelled pressing back' do
-    Rails.logger.info("New Item – Back")
+    Rails.logger.info('New Item – Back')
     visit '/items'
-    page.click_link("New Item")
-    page.click_link("Back")
-    page.has_content?("Listing Items")
+    page.click_link('New Item')
+    page.click_link('Back')
+    page.has_content?('Listing Items')
   end
 end
 
 describe 'Delete element', type: :feature do
+  let(:item) do
+    Item.new(name: 'xName', description: 'xDescription')
+  end
+
   before(:each) do
     Capybara.current_driver = Capybara.javascript_driver
     time = Time.now
     item.name = 'xName' + time.inspect
     item.description = 'xDescription' + time.inspect
     createPage(item)
-    page.click_link("Back")
+    page.click_link('Back')
     find(:xpath, "//td[text()='"+item.name+"']/..//a[contains(text(), 'Destroy')]").click
   end
 
@@ -75,35 +79,39 @@ describe 'Delete element', type: :feature do
   end
 
   it 'is happy path' do
-    Rails.logger.info("Delete Item – Happy path")
+    Rails.logger.info('Delete Item – Happy path')
     page.driver.browser.switch_to.alert.accept
-    expect(page).to have_content("Item was successfully destroyed.")
+    expect(page).to have_content('Item was successfully destroyed.')
   end
 
   it 'is cancelled' do
-    Rails.logger.info("Destroy – Cancel")
+    Rails.logger.info('Destroy – Cancel')
     page.driver.browser.switch_to.alert.dismiss
     expect(page).to have_content(item.name)
   end
 end
 
 describe 'Edit element', type: :feature do
+  let(:item) do
+    Item.new(name: 'xName', description: 'xDescription')
+  end
+
   before(:each) do
     Capybara.current_driver = Capybara.javascript_driver
     time = Time.now
     item.name = 'xName' + time.inspect
     item.description = 'xDescription' + time.inspect
     createPage(item)
-    page.click_link("Back")
+    page.click_link('Back')
   end
 
   after(:each) do
-    deleteItem($name)
+    deleteItem(item)
   end
 
   it 'is happy path' do
-    Rails.logger.info("Edit Item – Happy path")
-    itemNew = item.clone
+    Rails.logger.info('Edit Item – Happy path')
+    itemNew = item.dup
     itemNew.name = itemNew.name + 'Edited'
     itemNew.description = itemNew.description + 'Edited'
     editItem(item, itemNew)
@@ -121,75 +129,75 @@ describe 'Edit element', type: :feature do
   end
 
   it 'has huge values' do
-    Rails.logger.info("Edit Item – Huge size")
-    itemNew = item.clone
+    Rails.logger.info('Edit Item – Huge size')
+    itemNew = item.dup
     itemNew.name = itemNew.name + '123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890'
     itemNew.description = itemNew.description + '123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890'
     editItem(item, itemNew)
-    expect(page).to have_content("Item was successfully updated.")
+    expect(page).to have_content('Item was successfully updated.')
     checkValid(itemNew)
   end
 
   it 'cancels editing' do
-    Rails.logger.info("Edit Item – Back")
+    Rails.logger.info('Edit Item – Back')
     find(:xpath, "//td[text()='" + item.name + "']/..//a[contains(text(), 'Edit')]").click
-    page.click_link("Back")
-    expect(page).to have_content("Listing Items")
+    page.click_link('Back')
+    expect(page).to have_content('Listing Items')
   end
 
   it 'shows the editing' do
-    Rails.logger.info("Edit Item – Show")
-    itemNew = item.clone
-    itemNew.name = itemNew.name + "Edited"
-    itemNew.description = itemNew.description + "Edited"
+    Rails.logger.info('Edit Item – Show')
+    itemNew = item.dup
+    itemNew.name = itemNew.name + 'Edited'
+    itemNew.description = itemNew.description + 'Edited'
     find(:xpath, "//td[text()='" + item.name + "']/..//a[contains(text(), 'Edit')]").click
     fillValues(itemNew)
-    page.click_link("Show")
+    page.click_link('Show')
     expect(page).to have_content(item.name)
     expect(page).to have_content(item.description)
   end
 end
 
 def fillValues(item)
-  Rails.logger.debug("Enter: fillValues - name: " + item.name + " - description: " + item.description)
+  Rails.logger.debug('Enter: fillValues - name: ' + item.name + ' - description: ' + item.description)
   page.fill_in('item_name', with: item.name)
   page.fill_in('item_description', with: item.description)
-  Rails.logger.debug("Exit: fillValues - name: " + item.name + " - description: " + item.description)
+  Rails.logger.debug('Exit: fillValues - name: ' + item.name + ' - description: ' + item.description)
 end
 
 def editItem(oldItem, newItem)
-  Rails.logger.debug("Enter: editItem - oldName: " + oldItem.name + " - oldDescription: " + oldItem.description + " - newName: " + newItem.name + " - newDescription: " + newItem.description)
+  Rails.logger.debug('Enter: editItem - oldName: ' + oldItem.name + ' - oldDescription: ' + oldItem.description + ' - newName: ' + newItem.name + ' - newDescription: ' + newItem.description)
   find(:xpath, "//td[text()='" + oldItem.name + "']/..//a[contains(text(), 'Edit')]").click
   fillValues(newItem)
-  page.click_button("Update Item")
-  Rails.logger.debug("Exit: editItem - oldName: " + oldItem.name + " - oldDescription: " + oldItem.description + " - newName: " + newItem.name + " - newDescription: " + newItem.description)
+  page.click_button('Update Item')
+  Rails.logger.debug('Exit: editItem - oldName: ' + oldItem.name + ' - oldDescription: ' + oldItem.description + ' - newName: ' + newItem.name + ' - newDescription: ' + newItem.description)
 end
 
 def createPage(item)
-  Rails.logger.debug("Enter: createPage - name: " + item.name + " - description: " + item.description)
+  Rails.logger.debug('Enter: createPage - name: ' + item.name + ' - description: ' + item.description)
   visit '/items'
-  page.click_link("New Item")
+  page.click_link('New Item')
   url = URI.parse(current_url)
-  expect(url.path).to eq("/items/new")
+  expect(url.path).to eq('/items/new')
   fillValues(item)
   page.click_button('Create Item')
-  Rails.logger.debug("Exit: createPage - name: " + item.name + " - description: " + item.description)
+  Rails.logger.debug('Exit: createPage - name: ' + item.name + ' - description: ' + item.description)
 end
 
 def checkValid(item)
-  Rails.logger.debug("Enter: checkValid - name: " + item.name.inspect + " - description: " + item.description.inspect)
-  expect(page).to have_content(name)
-  expect(page).to have_content(description)
-  Rails.logger.debug("Exit: checkValid - name: " + item.name.inspect + " - description: " + item.description.inspect)
+  Rails.logger.debug('Enter: checkValid - name: ' + item.name.inspect + ' - description: ' + item.description.inspect)
+  expect(page).to have_content(item.name)
+  expect(page).to have_content(item.description)
+  Rails.logger.debug('Exit: checkValid - name: ' + item.name.inspect + ' - description: ' + item.description.inspect)
 end
 
 def deleteItem(item)
   begin
-    visit "/items"
+    visit '/items'
     find(:xpath, "//td[contains(text(),'" + item.name + "')]/..//a[contains(text(), 'Destroy')]").click
     page.driver.browser.switch_to.alert.accept
-    Rails.logger.debug("Element " + item.name + " was deleted.")
+    Rails.logger.debug('Element ' + item.name + ' was deleted.')
   rescue Capybara::ElementNotFound
-    Rails.logger.debug("Unable to delete item")
+    Rails.logger.debug('Unable to delete item')
   end
 end
